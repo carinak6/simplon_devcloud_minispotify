2d7abec86aff99db85dd1e79c659ca1b2fba10ee
carinak6 <kassiscarina@gmail.com>
2021-01-16 21:56:53 +0100

    modification d affichage de liste des chansons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 //liste des chanson avec le titre, son fichier audio et l image qui l acompagne
 listeChanson=[
-['Queen - Bohemian Rhapsody','Bohemian Rhapsody.flac','queen.jpg'],
-["Johnny Hallyday - Allumer le feu","100 - Johnny Hallyday - Allumer le feu.mp3","johnny.jpg"],
-["Alain Souchon - Foule sentimentale","alain-souchon-foule-sentimentale.mp3","AlainSouchon.jpg"],
-["Le bolero de Ravel","maurice-ravel-bolero.mp3","bolero.jpg"], 
-["Pascal Obispo - Lucie", "pascal-obispo-lucie-clip-officiel.mp3","obispo.jpg"],
-["Polnareff - Lettre a France","polnareff-lettre-a-france-original-version.mp3","polnareff.jpg"]
+['Bohemian Rhapsody','Queen','Bohemian Rhapsody.flac','queen.jpg'],
+["Allumer le feu",'Johnny Hallyday',"100 - Johnny Hallyday - Allumer le feu.mp3","johnny.jpg"],
+["Foule sentimentale","Alain Souchon","alain-souchon-foule-sentimentale.mp3","AlainSouchon.jpg"],
+["Le bolero de Ravel","-","maurice-ravel-bolero.mp3","bolero.jpg"], 
+["Lucie","Pascal Obispo", "pascal-obispo-lucie-clip-officiel.mp3","obispo.jpg"],
+["Lettre a France","Polnareff","polnareff-lettre-a-france-original-version.mp3","polnareff.jpg"]
 ];
 
 
 /************ generation de liste des chanson  dynamiquement **************/
-//on capte la balise ul
+//on capte la balise table                       
 listeChansons = document.querySelector("#listeMusique");
 
 //on declare la variable string qui recevra les balises pour ajouter à l ul capté avant
@@ -18,10 +18,13 @@ let listeMusique="";
 
 //on cree la liste des chanson
 for(let i = 0 ; i < listeChanson.length; i++){    
-    listeMusique += "<li id=\""+ i +"\">"+(i+1)+" - <i class=\"far fa-heart\"></i> <label class='chanson'>"+listeChanson[i][0]+"</label></li> ";
+    //listeMusique += "<li id=\""+ i +"\">"+(i+1)+" - <i class=\"far fa-heart\"></i> <label class='chanson'>"+listeChanson[i][0]+"</label></li> ";
+    
+    listeMusique += "<tr> <td class='nro' >"+ (i+1) +"</td><td class='coeur'><i class=\"far fa-heart\"></i></td><td class='chanson titre'>"+listeChanson[i][0]+"</td><td class='artiste'>"+ listeChanson[i][1]+ "</td><td class='temps'>3:00</td></tr>";
 }
+console.log(listeMusique)
 //on ajoute la variable avec les LI dans la balise UL#listeMusique
-listeChansons.innerHTML =listeMusique;
+listeChansons.innerHTML += "<tr><th class='nro'>#</th><th class='coeur'></th><th class='titre'>Titre</th><th class='artiste'>Artiste</th><th class='temps'><i class=\"fas fa-stopwatch\"></i></th></tr><tbody>"+ listeMusique +'</tbody>';
 
 
 /************* des label avec les chansons ****************/
@@ -38,8 +41,8 @@ listLabel.forEach((valeur, index)=>{
     //j applique à chaque label l'event click
     valeur.addEventListener("click", function(){ 
         changerCouleur(this);
-        
-        jouerChanson(listeChanson[index][1],listeChanson[index][2]);
+        console.log(this);
+        jouerChanson(listeChanson[index][2],listeChanson[index][3]);
     });
    
 });
@@ -55,7 +58,12 @@ listCoeur.forEach((valeur, index)=>{
     //j applique à chaque icon l'event click pour changer sa class et par ende l image
     valeur.addEventListener("click", function(elem){ 
         console.log(elem.target);
-        elem.target.setAttribute('class','fas fa-heart')
+        if(elem.target.className == 'fas fa-heart' ){
+            elem.target.setAttribute('class','far fa-heart')
+        }else{
+            elem.target.setAttribute('class','fas fa-heart')
+        }
+        
     });
    
 });
@@ -86,8 +94,10 @@ function changerCouleur(itemChoisie) {
     /* pour enlever le background des items deja selectione
     mais peut etre on peut utiliser un changement de nom de class qui aura le background souhaité
     */
-    toutes = document.querySelectorAll("ul#listeMusique>li");
+    toutes = document.querySelectorAll("table#listeMusique>tbody>tr");
+    console.log(toutes)
     toutes.forEach(element => {
+        console.log(element);        
         element.style.backgroundColor="";
     });
     
